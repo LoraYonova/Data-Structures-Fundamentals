8f0c783b9283b38df37dca94198e95e1227079b4
Лора Йонова <79514429+LoraYonova@users.noreply.github.com>
2022-09-18 13:39:32 +0300

    Implement BalancedParentheses

--- Linear Data Structures - Exercise/src/main/java/implementations/BalancedParentheses.java
+++ Linear Data Structures - Exercise/src/main/java/implementations/BalancedParentheses.java
@@ -2,6 +2,8 @@ package implementations;
 
 import interfaces.Solvable;
 
+import java.util.Stack;
+
 public class BalancedParentheses implements Solvable {
     private String parentheses;
 
@@ -11,6 +13,26 @@ public class BalancedParentheses implements Solvable {
 
     @Override
     public Boolean solve() {
-        return null;
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < this.parentheses.length(); i++) {
+            char c = parentheses.charAt(i);
+            if (c == '[' || c == '(' || c == '{') {
+                stack.push(c);
+            } else if (c == ']') {
+                if (stack.isEmpty() || stack.pop() != '[') {
+                    return false;
+                }
+            } else if (c == ')') {
+                if (stack.isEmpty() || stack.pop() != '(') {
+                    return false;
+                }
+            } else if (c == '}') {
+                if (stack.isEmpty() || stack.pop() != '{') {
+                    return false;
+                }
+            }
+
+        }
+        return stack.isEmpty();
     }
 }
