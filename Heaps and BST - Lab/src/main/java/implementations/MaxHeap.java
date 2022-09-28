d95efed4cb14406f0dd649de135bc42b44395cd8
Лора Йонова <79514429+LoraYonova@users.noreply.github.com>
2022-09-28 12:47:56 +0300

    Implement MaxHeap

--- Heaps and BST - Lab/src/main/java/implementations/MaxHeap.java
+++ Heaps and BST - Lab/src/main/java/implementations/MaxHeap.java
@@ -8,18 +8,50 @@ import java.util.List;
 
 public class MaxHeap<E extends Comparable<E>> implements Heap<E> {
 
+    private List<E> elements;
+
+    public MaxHeap() {
+        this.elements = new ArrayList<>();
+    }
+
     @Override
     public int size() {
-        return 0;
+        return this.elements.size();
     }
 
     @Override
     public void add(E element) {
 
+        this.elements.add(element);
+        this.heapifyUp(this.size() - 1);
+    }
+
+    private void heapifyUp(int index) {
+
+        while (index > 0 && less(getParentIndex(index), index)) {
+            Collections.swap(this.elements, getParentIndex(index), index);
+            index = getParentIndex(index);
+        }
+
+    }
+
+    private boolean less(int parentIndex, int childrenIndex) {
+        return getAt(parentIndex).compareTo(getAt(childrenIndex)) < 0;
+    }
+
+    private E getAt(int index) {
+        return this.elements.get(index);
+    }
+
+    private int getParentIndex(int index) {
+        return (index - 1) / 2;
     }
 
     @Override
     public E peek() {
-        return null;
+        if (this.size() == 0) {
+            throw new IllegalStateException("Heap is empty upon peek attempt");
+        }
+        return this.elements.get(0);
     }
 }
