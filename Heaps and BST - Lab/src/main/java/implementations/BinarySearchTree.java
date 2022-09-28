3e88380ac48e8fd8a8e24dccf63aa36314716115
Лора Йонова <79514429+LoraYonova@users.noreply.github.com>
2022-09-28 15:49:07 +0300

    Implement PriorityQueue and BinarySearchTree

--- Heaps and BST - Lab/src/main/java/implementations/BinarySearchTree.java
+++ Heaps and BST - Lab/src/main/java/implementations/BinarySearchTree.java
@@ -4,38 +4,114 @@ import interfaces.AbstractBinarySearchTree;
 
 public class BinarySearchTree<E extends Comparable<E>> implements AbstractBinarySearchTree<E> {
 
+    private Node<E> root;
+    private Node<E> leftChild;
+    private Node<E> rightChild;
+
+    public BinarySearchTree() {}
+    public BinarySearchTree(Node<E> root) {
+            this.copy(root);
+    }
+
+    private void copy(Node<E> node) {
+        if (node != null) {
+            this.insert(node.value);
+            this.copy(node.leftChild);
+            this.copy(node.rightChild);
+        }
+    }
+
+
     @Override
     public void insert(E element) {
+        Node<E> newNode = new Node<>(element);
+        if (this.root == null) {
+            this.root = newNode;
+        } else {
+            Node<E> current = this.root;
+            Node<E> prev = current;
+            while (current != null) {
+                prev = current;
+                if (less(element, current.value)) {
+                    current = current.leftChild;
+                } else if (greater(element, current.value)) {
+                    current = current.rightChild;
+                } else {
+                    return;
+                }
+            }
+            current = newNode;
+            if (less(element, prev.value)) {
+                prev.leftChild = newNode;
+            } else if (greater(element, prev.value)) {
+                prev.rightChild = newNode;
+            }
 
+        }
     }
+
+    private boolean less(E first, E second) {
+        return first.compareTo(second) < 0;
+    }
+
+    private boolean greater(E first, E second) {
+        return first.compareTo(second) > 0;
+    }
+
+//    private boolean equal(E first, E second) {
+//        return first.compareTo(second) == 0;
+//    }
 
     @Override
     public boolean contains(E element) {
+        Node<E> current = this.root;
+        while (current != null) {
+            if (less(element, current.value)) {
+                current = current.leftChild;
+            } else if (greater(element, current.value)) {
+                current = current.rightChild;
+            } else {
+                return true;
+            }
+        }
+
         return false;
     }
 
     @Override
     public AbstractBinarySearchTree<E> search(E element) {
-        return null;
+
+        AbstractBinarySearchTree<E> tree = new BinarySearchTree<>();
+        Node<E> current = this.root;
+        while (current != null) {
+            if (less(element, current.value)) {
+                current = current.leftChild;
+            } else if (greater(element, current.value)) {
+                current = current.rightChild;
+            } else {
+                return new BinarySearchTree<E>(current);
+            }
+        }
+        return tree;
     }
 
     @Override
     public Node<E> getRoot() {
-        return null;
+        return this.root;
     }
 
     @Override
     public Node<E> getLeft() {
-        return null;
+        return this.leftChild;
     }
 
     @Override
     public Node<E> getRight() {
-        return null;
+        return this.rightChild;
     }
 
     @Override
     public E getValue() {
-        return null;
+        return this.root.value;
     }
 }
--- Heaps and BST - Lab/src/main/java/implementations/PriorityQueue.java
+++ Heaps and BST - Lab/src/main/java/implementations/PriorityQueue.java
@@ -7,24 +7,80 @@ import java.util.Collections;
 import java.util.List;
 
 public class PriorityQueue<E extends Comparable<E>> implements AbstractQueue<E> {
+    private List<E> elements;
+
+    public PriorityQueue() {
+        this.elements = new ArrayList<>();
+    }
 
     @Override
     public int size() {
-        return 0;
+        return this.elements.size();
     }
 
     @Override
     public void add(E element) {
-
+        this.elements.add(element);
+        this.heapifyDown(this.size() - 1);
     }
 
     @Override
     public E peek() {
-        return null;
+        ensureNonEmpty();
+        return getAt(0);
     }
 
     @Override
     public E poll() {
-        return null;
+        ensureNonEmpty();
+        E element = this.elements.get(0);
+        Collections.swap(this.elements, 0, this.elements.size() - 1);
+        this.elements.remove(this.size() - 1);
+        this.heapifyDown(0);
+        return element;
+    }
+
+    private E getLeftChild(int index) {
+        return this.elements.get(this.getLeftChildIndex(index));
+    }
+
+    private E getRightChild(int index) {
+        return this.elements.get(this.getRightChildIndex(index));
+    }
+
+    private int getLeftChildIndex(int index) {
+        return 2 * index + 1;
+    }
+
+    private int getRightChildIndex(int index) {
+        return 2 * index + 2;
+    }
+
+    private void heapifyDown(int index) {
+     while (getLeftChildIndex(index) < this.size() && less(index, getLeftChildIndex(index))) {
+         int child = getLeftChildIndex(index);
+         int rightIndex = getRightChildIndex(index);
+
+         if (rightIndex < this.size() && less(child, rightIndex)) {
+             child = rightIndex;
+         }
+
+         Collections.swap(this.elements, child, index);
+         index = child;
+     }
+    }
+
+    private boolean less(int first, int second) {
+        return getAt(first).compareTo(getAt(second)) < 0;
+    }
+
+    private E getAt(int index) {
+        return this.elements.get(index);
+    }
+
+    private void ensureNonEmpty() {
+        if (this.size() == 0) {
+            throw new IllegalStateException("Heap is empty upon peek/poll attempt");
+        }
     }
 }
