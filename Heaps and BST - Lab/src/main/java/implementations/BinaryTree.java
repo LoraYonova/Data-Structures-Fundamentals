cb6945e6142b8f39d863706ea80f002dae5f1fed
Лора Йонова <79514429+LoraYonova@users.noreply.github.com>
2022-09-28 11:32:53 +0300

    Initial commit

---
+++ Heaps and BST - Lab/src/main/java/implementations/BinaryTree.java
@@ -0,0 +1,87 @@
+package implementations;
+
+import interfaces.AbstractBinaryTree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class BinaryTree<E> implements AbstractBinaryTree<E> {
+
+    private E key;
+    private BinaryTree<E> left;
+    private BinaryTree<E> right;
+
+    public BinaryTree(E key, BinaryTree<E> left, BinaryTree<E> right) {
+        this.key = key;
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public E getKey() {
+        return this.key;
+    }
+
+    @Override
+    public AbstractBinaryTree<E> getLeft() {
+        return this.left;
+    }
+
+    @Override
+    public AbstractBinaryTree<E> getRight() {
+        return this.right;
+    }
+
+    @Override
+    public void setKey(E key) {
+        this.key = key;
+    }
+
+    @Override
+    public String asIndentedPreOrder(int indent) {
+        StringBuilder result = new StringBuilder();
+
+        result.append(createPadding(indent)).append(getKey());
+
+        if (getLeft() != null) {
+            result.append(System.lineSeparator()).append(getLeft().asIndentedPreOrder(indent + 2));
+        }
+
+        if (getRight() != null) {
+            result.append(System.lineSeparator()).append(getRight().asIndentedPreOrder(indent + 2));
+        }
+
+        return result.toString();
+    }
+
+    private String createPadding(int indent) {
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < indent; i++) {
+            builder.append(" ");
+        }
+
+        return builder.toString();
+    }
+
+    @Override
+    public List<AbstractBinaryTree<E>> preOrder() {
+        return null;
+    }
+
+    @Override
+    public List<AbstractBinaryTree<E>> inOrder() {
+        return null;
+    }
+
+    @Override
+    public List<AbstractBinaryTree<E>> postOrder() {
+        return null;
+    }
+
+    @Override
+    public void forEachInOrder(Consumer<E> consumer) {
+
+    }
+}
