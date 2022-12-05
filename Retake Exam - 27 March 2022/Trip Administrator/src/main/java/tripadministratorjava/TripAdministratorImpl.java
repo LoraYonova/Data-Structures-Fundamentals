61d1e610ce1bc2ddded27fa8f90a6a9414e4578d
Лора Йонова <79514429+LoraYonova@users.noreply.github.com>
2022-12-05 09:27:37 +0200

    Implement TripAdministratorImpl

--- Retake Exam - 27 March 2022/Trip Administrator/src/main/java/tripadministratorjava/TripAdministratorImpl.java
+++ Retake Exam - 27 March 2022/Trip Administrator/src/main/java/tripadministratorjava/TripAdministratorImpl.java
@@ -1,61 +1,106 @@
 package tripadministratorjava;
 
-import java.util.Collection;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class TripAdministratorImpl implements TripAdministrator {
 
+    private Map<Company, List<Trip>> tripAdministrator;
+    private List<Trip> trip;
+
+    public TripAdministratorImpl() {
+        tripAdministrator = new HashMap<>();
+        trip = new ArrayList<>();
+    }
+
     @Override
     public void addCompany(Company c) {
-
+        if (tripAdministrator.containsKey(c)) {
+            throw new IllegalArgumentException();
+        }
+        tripAdministrator.put(c, new ArrayList<>());
     }
 
     @Override
     public void addTrip(Company c, Trip t) {
 
+        if (!tripAdministrator.containsKey(c)) {
+            throw new IllegalArgumentException();
+        }
+
+        if (trip.contains(t)) {
+            throw new IllegalArgumentException();
+        }
+
+        tripAdministrator.get(c).add(t);
+        trip.add(t);
     }
 
     @Override
     public boolean exist(Company c) {
-        return false;
+        return tripAdministrator.containsKey(c);
     }
 
     @Override
     public boolean exist(Trip t) {
-        return false;
+        return trip.contains(t);
     }
 
     @Override
     public void removeCompany(Company c) {
 
+        if (!tripAdministrator.containsKey(c)) {
+            throw new IllegalArgumentException();
+        }
+
+        List<Trip> trips = tripAdministrator.get(c);
+        tripAdministrator.remove(c);
+        trip.removeAll(trips);
     }
 
     @Override
     public Collection<Company> getCompanies() {
-        return null;
+        return tripAdministrator.keySet();
     }
 
     @Override
     public Collection<Trip> getTrips() {
-        return null;
+        return trip;
     }
 
     @Override
     public void executeTrip(Company c, Trip t) {
 
+        if (!tripAdministrator.containsKey(c)) {
+            throw new IllegalArgumentException();
+        }
+        List<Trip> trips = tripAdministrator.get(c);
+
+        if(!trips.contains(t)) {
+            throw new IllegalArgumentException();
+        }
+
+        tripAdministrator.get(c).remove(t);
+        trip.remove(t);
     }
 
     @Override
     public Collection<Company> getCompaniesWithMoreThatNTrips(int n) {
-        return null;
+
+       return tripAdministrator.entrySet()
+                .stream().filter(companyListEntry -> companyListEntry.getValue().size() > n)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+
     }
 
     @Override
     public Collection<Trip> getTripsWithTransportationType(Transportation t) {
-        return null;
+        return trip.stream().filter(tr -> tr.getTransportation().equals(t)).collect(Collectors.toList());
     }
 
     @Override
     public Collection<Trip> getAllTripsInPriceRange(int lo, int hi) {
-        return null;
+        return trip.stream().filter(tr -> tr.price >= lo && tr.price <= hi).collect(Collectors.toList());
     }
 }
