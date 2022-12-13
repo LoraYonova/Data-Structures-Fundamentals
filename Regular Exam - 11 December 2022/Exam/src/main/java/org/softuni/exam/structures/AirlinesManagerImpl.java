202e7273347169b5362b9ab0105b200ea619e311
Лора Йонова <79514429+LoraYonova@users.noreply.github.com>
2022-12-13 14:10:05 +0200

    Implement AirlinesManagerImpl

--- Regular Exam - 11 December 2022/Exam/src/main/java/org/softuni/exam/structures/AirlinesManagerImpl.java
+++ Regular Exam - 11 December 2022/Exam/src/main/java/org/softuni/exam/structures/AirlinesManagerImpl.java
@@ -3,40 +3,79 @@ package org.softuni.exam.structures;
 import org.softuni.exam.entities.Airline;
 import org.softuni.exam.entities.Flight;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 public class AirlinesManagerImpl implements AirlinesManager {
+
+    private Map<Airline, List<Flight>> airlineManager;
+    private List<Flight> flights;
+
+    public AirlinesManagerImpl() {
+        this.airlineManager = new LinkedHashMap<>();
+        this.flights = new ArrayList<>();
+    }
+
+
     @Override
     public void addAirline(Airline airline) {
-
+        airlineManager.putIfAbsent(airline, new ArrayList<>());
     }
 
     @Override
     public void addFlight(Airline airline, Flight flight) {
 
+        if (!airlineManager.containsKey(airline)) {
+            throw new IllegalArgumentException();
+        }
+
+        airlineManager.get(airline).add(flight);
+        flights.add(flight);
     }
 
     @Override
     public boolean contains(Airline airline) {
-        return false;
+        return airlineManager.containsKey(airline);
     }
 
     @Override
     public boolean contains(Flight flight) {
-        return false;
+        return flights.contains(flight);
     }
 
     @Override
     public void deleteAirline(Airline airline) throws IllegalArgumentException {
 
+        if (!airlineManager.containsKey(airline)) {
+            throw new IllegalArgumentException();
+        }
+
+        List<Flight> flights = airlineManager.get(airline);
+        airlineManager.remove(airline);
+        this.flights.removeAll(flights);
     }
 
     @Override
     public Iterable<Flight> getAllFlights() {
-        return null;
+        return flights;
     }
 
     @Override
     public Flight performFlight(Airline airline, Flight flight) throws IllegalArgumentException {
-        return null;
+        if (!airlineManager.containsKey(airline)) {
+            throw new IllegalArgumentException();
+        }
+
+        List<Flight> flights = airlineManager.get(airline);
+        if (!flights.contains(flight)) {
+            throw new IllegalArgumentException();
+        }
+
+        Flight perform = this.flights.stream().filter(f -> f.getId().equals(flight.getId())).findFirst().get();
+        perform.setCompleted(true);
+        return perform;
     }
 
     @Override
